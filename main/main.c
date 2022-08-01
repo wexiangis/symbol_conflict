6670458f4d85ce256cda68268d734b07fdb60969
wexiangis <wexiangis@qq.com>
2022-08-01 17:06:01 +0800

    示例精简

--- main/main.c
+++ main/main.c
@@ -5,10 +5,9 @@
 #include "lib2.h"
 #include "symbolmatch.h"
 
-typedef void(*LIB_PRINT)();
-
 int main()
 {
+
     /* 两个库都调用一次,防止被优化而没有链接库 */
     lib1_init();
     lib2_init();
@@ -16,14 +15,12 @@ int main()
     /* 第一次调用,无法区分是谁的函数 */
     libx_print();
 
-    LIB_PRINT lib1_print = (LIB_PRINT)sm_getFunAddr("libx_print", "lib1.so");
-    LIB_PRINT lib2_print = (LIB_PRINT)sm_getFunAddr("libx_print", "lib2.so");
-
     /* 第二次调用,区分来自不同动态库的函数 */
-    if (lib1_print)
-        lib1_print();
-    if (lib2_print)
-        lib2_print();
+    void(*lib1_print)() = (void(*)())sm_getFunAddr("libx_print", "lib1.so");
+    void(*lib2_print)() = (void(*)())sm_getFunAddr("libx_print", "lib2.so");
+
+    lib1_print();
+    lib2_print();
 
     return 0;
 }
